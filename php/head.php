d13a708273a6488f743f04c8935f005256be5385
Hendrik <hendrik.punke@uni-oldenburg.de>
2024-05-08 17:30:56 +0200

    Change head.php

--- php/head.php
+++ php/head.php
@@ -2,8 +2,8 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <meta name="author" content="Peper, Punke, Kemper"
-    <meta name="description" content="SetMatch"
+    <meta name="author" content="Peper, Punke, Kemper">
+    <meta name="description" content="SetMatch">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SetMatch</title>
     <link rel="stylesheet" href="/css/style.css">
