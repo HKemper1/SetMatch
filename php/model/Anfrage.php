712ed38f50ba1e3dcd2c34307e0377528ec77d67
Hendrik <hendrik.punke@uni-oldenburg.de>
2024-05-08 18:13:53 +0200

    Implement base Structure from Anfrage.php

--- php/model/Anfrage.php
+++ php/model/Anfrage.php
@@ -1,4 +1,39 @@
 <?php
+class Anfrage 
+{
+    private $id;
+    private $teamname;
+    private $email;
+    private $empfaenger;
+
+    /* Konstruktor Anfrage */
+    public function __construct($id, $teamname, $email, $empfaenger){
+        $this->id = $id;
+        $this->teamname = $teamname;
+        $this->email = $email;
+        $this->empfaenger = $empfaenger;
+    }
+
+    /* Getter und Setter */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTeamname()
+    {
+        return $this->teamname;
+    }
 
 
+    public function getEMail()
+    {
+        return $this->email;
+    }
+
+    public function getEmpfaenger()
+    {
+        return $this->empfaenger;
+    }
+}
 ?>
