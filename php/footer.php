fa3b19ca8d4a3dfc79ff07a6e43dcfd3368c5c63
Hendrik <hendrik.punke@uni-oldenburg.de>
2024-05-08 17:41:39 +0200

    Change footer and hea

--- php/footer.php
+++ php/footer.php
@@ -2,5 +2,4 @@
     <a href="impressum.php">Impressum</a>
     <a href="datenschutz.php">Datenschutz</a>
     <a href="nutzerbedingungen.php">Nutzungsbedingungen</a>
-
 </footer>
--- php/head.php
+++ php/head.php
@@ -7,6 +7,4 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SetMatch</title>
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="css/style.css">
-
 </head>
