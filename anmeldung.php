e04b55ef34698e8e668249b35ff049ee26e6c149
Hendrik <hendrik.punke@uni-oldenburg.de>
2024-05-13 11:26:29 +0200

    anmeldung.php lauffähig gemacht und path.php hinzugefügt

--- anmeldung.php
+++ anmeldung.php
@@ -1,37 +1,44 @@
-<?php include_once "php/head.php" ?>
-
+<?php
+    if (!isset($abs_path)) {
+        require_once "path.php";
+    }
+?>
 <?php
     require_once $abs_path . "/php/include/head.php";
 ?>
 <body>
-<main>
-    <div class="container">
-    <section>
-        <h1>Anmelden</h1>
+    <?php
+        require_once $abs_path . "/php/include/nav.php";
+    ?>
+    <main>
+        <div class="container">
+        <section>
+            <h1>Anmelden</h1>
 
-        <form action="index.php" method="POST">
-            <div>
-                <label for="teamname">Teamname</label>
+            <form action="index.php" method="POST">
                 <div>
-                    <input type="text" id="teamname" name="teamname" maxlength="100" required>
+                    <label for="teamname">Teamname</label>
+                    <div>
+                        <input type="text" id="teamname" name="teamname" maxlength="100" required>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label for="password">Passwort:</label>
                 <div>
-                    <input type="password" id="password" name="password" maxlength="100" required>
+                    <label for="password">Passwort:</label>
+                    <div>
+                        <input type="password" id="password" name="password" maxlength="100" required>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <a href="registrierung.php" class="button">Registrieren</a>
-                <button class="button" type="submit">Anmelden </button>
-            </div>
-        </form>
-
-    </section>
-    </div>
-</main>
-    <?php include_once "php/footer.php" ?>
+                <div>
+                    <a href="registrierung.php" class="button">Registrieren</a>
+                    <button class="button" type="submit">Anmelden </button>
+                </div>
+            </form>
+        </section>
+        </div>
+    </main>
+    <?php 
+        include_once $abs_path . "/php/include/footer.php";
+    ?>
 </body>
 
 </html>
---
+++ path.php
@@ -0,0 +1,3 @@
+<?php
+$abs_path = __DIR__;
+?>
