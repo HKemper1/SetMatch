0a1a7f4b6a91328e8338d39f0699dfcbe4e81bb8
Hendrik <hendrik.punke@uni-oldenburg.de>
2024-05-29 22:17:30 +0200

    changes

--- php/include/header.php
+++ php/include/header.php
@@ -1,7 +1,7 @@
 <header class="header">
     
     <nav class="nav normalerText">
-    <img src="././images/basis_images/logo-80x80.png"
+    <img src="../SetMatch/images/basis_images/logo-80x80.png"
                 srcset="logo-160x160 2x"
                 alt="SetMatch"
                 width="80"
